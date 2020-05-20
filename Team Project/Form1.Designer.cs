54efefa41960fd574a7ce09a2a655c860961f87a
nagyaong <o_nag@naver.com>
2020-05-20 15:49:14 +0900

    초기 커밋.

---
+++ Team Project/Form1.Designer.cs
@@ -0,0 +1,440 @@
+﻿namespace Team_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list_subject = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // list_subject
+            // 
+            this.list_subject.FormattingEnabled = true;
+            this.list_subject.ItemHeight = 18;
+            this.list_subject.Location = new System.Drawing.Point(684, 122);
+            this.list_subject.Name = "list_subject";
+            this.list_subject.Size = new System.Drawing.Size(382, 364);
+            this.list_subject.TabIndex = 0;
+            this.list_subject.DoubleClick += new System.EventHandler(this.list_subject_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(265, 36);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "학년";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 18);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "학기";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(560, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 18);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "최대 학점";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(315, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 28);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(472, 33);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(66, 28);
+            this.textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(661, 30);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 28);
+            this.textBox4.TabIndex = 8;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(183, 122);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(300, 122);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(22, 21);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(403, 122);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(22, 21);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(516, 122);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(22, 21);
+            this.checkBox4.TabIndex = 13;
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(624, 122);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(22, 21);
+            this.checkBox5.TabIndex = 14;
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.Location = new System.Drawing.Point(12, 166);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Size = new System.Drawing.Size(80, 22);
+            this.checkBox6.TabIndex = 15;
+            this.checkBox6.Text = "1교시";
+            this.checkBox6.UseVisualStyleBackColor = true;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.Location = new System.Drawing.Point(12, 408);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Size = new System.Drawing.Size(80, 22);
+            this.checkBox7.TabIndex = 16;
+            this.checkBox7.Text = "5교시";
+            this.checkBox7.UseVisualStyleBackColor = true;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.Location = new System.Drawing.Point(12, 352);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Size = new System.Drawing.Size(80, 22);
+            this.checkBox8.TabIndex = 17;
+            this.checkBox8.Text = "4교시";
+            this.checkBox8.UseVisualStyleBackColor = true;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.Location = new System.Drawing.Point(12, 292);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Size = new System.Drawing.Size(80, 22);
+            this.checkBox9.TabIndex = 18;
+            this.checkBox9.Text = "3교시";
+            this.checkBox9.UseVisualStyleBackColor = true;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.Location = new System.Drawing.Point(12, 232);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Size = new System.Drawing.Size(80, 22);
+            this.checkBox10.TabIndex = 19;
+            this.checkBox10.Text = "2교시";
+            this.checkBox10.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(785, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 18);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "이수 학점";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(890, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 28);
+            this.textBox1.TabIndex = 21;
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.Location = new System.Drawing.Point(12, 466);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Size = new System.Drawing.Size(80, 22);
+            this.checkBox11.TabIndex = 22;
+            this.checkBox11.Text = "6교시";
+            this.checkBox11.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(143, 539);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(395, 26);
+            this.comboBox1.TabIndex = 23;
+            this.comboBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.comboBox1_MouseDoubleClick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 542);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 18);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "내 선택 과목";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "컴퓨터정보공학부",
+            "소프트웨어학부",
+            "정보융합학부"});
+            this.comboBox2.Location = new System.Drawing.Point(110, 33);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(149, 26);
+            this.comboBox2.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(38, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 18);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "전공";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(890, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 36);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "확인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(563, 536);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 40);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "지우기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(140, 122);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 18);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "월";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(249, 122);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(26, 18);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "화";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(357, 122);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(26, 18);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "수";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(469, 122);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(26, 18);
+            this.label10.TabIndex = 32;
+            this.label10.Text = "목";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(578, 122);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(26, 18);
+            this.label11.TabIndex = 33;
+            this.label11.Text = "금";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(796, 533);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 42);
+            this.button3.TabIndex = 34;
+            this.button3.Text = "시간표 저장";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1095, 611);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox11);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox10);
+            this.Controls.Add(this.checkBox9);
+            this.Controls.Add(this.checkBox8);
+            this.Controls.Add(this.checkBox7);
+            this.Controls.Add(this.checkBox6);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.list_subject);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox list_subject;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Team Project/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+
+namespace Team_Project
+{
+    public partial class Form1 : Form
+    {
+       // DataGridTextBoxColumn textBoxColumn = new DataGridTextBoxColumn();
+        Button[] btn;
+        public Form1()
+        {
+            InitializeComponent();
+            //datagridview_make();
+            this.Load += button;
+        }
+        private void button(object sender, EventArgs e)
+        {
+            int x = 68;
+            int y = 102;
+            btn = new Button[5*6];
+            int j = 0;
+            for (int i = 0; i < 5*6; i++)
+            {
+                btn[i] = new Button();
+                btn[i].Parent = this;
+                btn[i].Text = "";
+                btn[i].Size = new Size(79, 40);
+                btn[i].Location = new Point(x, y);
+                int k = (i ) / 5+1;
+                btn[i].Name = i.ToString();// ((char)(j + 65)).ToString() + k.ToString();
+                //btn[i].Text = btn[i].Name;
+                j++;
+                
+               // btn[i].BackColor = Color.LightGray;
+                //btn[i].ForeColor = Color.LightGray;
+                btn[i].MouseDown += new MouseEventHandler(btn_Click);
+
+                x += 79;
+                if ((i + 1) % 5 == 0)
+                {
+                    y += 40;
+                    x = 68;
+                    j = 0;
+                }
+               /* panel1.Controls.Add(btn[i]);
+                panel1.Visible = false;
+                panel1.BackColor = Color.Transparent;*/
+                this.Controls.Add(btn[i]);
+
+            }
+        }
+        public void btn_Click(object sender, MouseEventArgs e)
+        {
+            /*Button btnn = sender as Button;
+            int index = int.Parse(btnn.Name);
+
+            if (btnn.Text != "")
+                btnn.Text = "";*///이렇게 하면 같은 과목 다른시간이 남아있게 됨
+        }
+
+       /* public void datagridview_make()
+        {
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+            dataGridView1.Rows.Add();
+        }*/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /* string str_FilePath = "SW_sample.xlxs";
+             string str_Provider = "Provider=Microsoft.ACE.OLEDB.12.0; Data Source=" + str_FilePath + @";Extended Properties='Excel 12.0;HDR=No'";
+
+             OleDbConnection oleDb_Conn = new OleDbConnection(str_Provider);
+             oleDb_Conn.Open();
+
+             string str_Query = "SELECT * FROM [Sheet1$]";
+             OleDbCommand oleDb_Comm = new OleDbCommand(str_Query, oleDb_Conn);
+             OleDbDataAdapter data_Adapter = new OleDbDataAdapter(oleDb_Comm);
+             System.Data.DataTable table = new DataTable();
+             data_Adapter.Fill(table);
+
+             oleDb_Conn.Close();
+
+
+             list_subject.Items.Clear();
+
+             foreach(DataRow row in table.Rows)
+             {
+                 list_subject.Items.Add(row.ToString());
+             }
+             */
+            read();
+
+
+        }
+
+        private void read()
+        {
+            Encoding encode = System.Text.Encoding.GetEncoding("ks_c_5601-1987");
+            FileStream stream = File.Open("sample.csv", FileMode.Open, FileAccess.Read);
+
+            using (var csv = new StreamReader(stream,encode))
+            {
+                csv.ReadLine();
+                //string line;
+               /* while (!csv.EndOfStream)
+                {
+                    list_subject.Items.Add(csv.ReadLine());
+                }
+                */
+                List<string> file = new List<string>();
+                while (!csv.EndOfStream)
+                {
+                    file.Add(csv.ReadLine());
+                }
+                foreach (string item in file)
+                {
+                    list_subject.Items.Add(item);
+                }
+            }
+        }
+
+        private void list_subject_DoubleClick(object sender, EventArgs e)
+        {
+            string item= list_subject.SelectedItem.ToString();
+            char[] num=new char[4];
+            num = item.Substring(2, 4).ToCharArray();
+
+            int n = (int)num[0] - 65 + (((int)num[1] - 49) * 5);
+            btn[n].Text = item.Substring(7, 6).Replace(",", "");
+
+             n = (int)num[2] - 65 + (((int)num[3] - 49) * 5);
+            btn[n].Text = item.Substring(7, 6).Replace(",","");
+
+            comboBox1.Items.Add(item);
+        }
+
+        private void comboBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+           
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string item = comboBox1.SelectedItem.ToString();
+            char[] num = new char[4];
+            num = item.Substring(2, 4).ToCharArray();
+
+            int n = (int)num[0] - 65 + (((int)num[1] - 49) * 5);
+            btn[n].Text = "";
+
+            n = (int)num[2] - 65 + (((int)num[3] - 49) * 5);
+            btn[n].Text = "";
+            comboBox1.Items.Remove(item);
+            comboBox1.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(79*5, 40*6);
+            int x = 0;
+            int y = 0;
+            int i = 0;
+            foreach(Control c in btn)
+            {
+                c.DrawToBitmap(bmp,new Rectangle(x,y, 79, 40));
+                x += 79;
+                if ((i + 1) % 5 == 0)
+                {
+                    y += 40;
+                    x = 0;
+                    
+                }
+                i++;
+            }
+            //this.panel1.DrawToBitmap(bmp, new Rectangle(0, 0, this.panel1.Width, this.panel1.Height));
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "JPG File(.jpg)|*.jpg|Bitmap File (.bmp)|*.bmp|PNG File (.png)|*.png|GIF File (.gif)|*.gif"; //저장할 확장자명 
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = saveFileDialog1.FileName;
+                bmp.Save(filename, System.Drawing.Imaging.ImageFormat.Jpeg);
+                MessageBox.Show("저장되었습니다");
+            }
+        }
+    }
+}
